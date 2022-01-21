9a7648ed150a60b909607aba0e17b3290ef99903
Gencehan <gencehangurcam@gmail.com>
2022-01-21 09:25:50 +0100

    part of portfolio done success

---
+++ resources/views/components/services.blade.php
@@ -0,0 +1,175 @@
+<!--/ Section Services Star /-->
+<section id="services" class="services-mf route">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="title-box text-center">
+                    <h3 class="title-a">
+                        Services
+                    </h3>
+                    <p class="subtitle-a">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    </p>
+                    <div class="line-mf"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="ion-monitor"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">Web Design</h2>
+                        <p class="s-description text-center">
+                            Que ce soit pour creer un simple logo ou jusqu'à une pub dynamique.
+                            {{-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat!
+                            Quia,
+                            provident vitae! Magni
+                            tempora perferendis eum non provident. --}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="ion-code-working"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">Web Development</h2>
+                        <p class="s-description text-center">
+                            Application mobile, site d'e-commerce, blog, ou tout projet personalisé.
+                            {{-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat!
+                            Quia,
+                            provident vitae! Magni
+                            tempora perferendis eum non provident. --}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="ion-camera"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">Photography</h2>
+                        <p class="s-description text-center">
+                            Photos professionnels, ou clip vidéo
+                            {{-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat!
+                            Quia,
+                            provident vitae! Magni
+                            tempora perferendis eum non provident. --}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="ion-android-phone-portrait"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">Responsive Design</h2>
+                        <p class="s-description text-center">
+                            Site mobile, amélioration de votre site aussi vieux soit il, pour le rendre responsive.
+                            {{-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat!
+                            Quia,
+                            provident vitae! Magni
+                            tempora perferendis eum non provident. --}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="ion-paintbrush"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">Graphic Design</h2>
+                        <p class="s-description text-center">
+                            Nous créons des enseignes, logos sur mesure, bannière, etc
+                            {{-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat!
+                            Quia,
+                            provident vitae! Magni
+                            tempora perferendis eum non provident. --}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-box">
+                    <div class="service-ico">
+                        <span class="ico-circle"><i class="ion-stats-bars"></i></span>
+                    </div>
+                    <div class="service-content">
+                        <h2 class="s-title">Marketing Services</h2>
+                        <p class="s-description text-center">
+                            Community manager, marketing de votre site, publicités facebook
+                            {{-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat!
+                            Quia,
+                            provident vitae! Magni
+                            tempora perferendis eum non provident. --}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/ Section Services End /-->
+
+<div class="section-counter paralax-mf bg-image" style="background-image: url(img/counters-bg.jpg)">
+    <div class="overlay-mf"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 col-lg-3">
+                <div class="counter-box">
+                    <div class="counter-ico">
+                        <span class="ico-circle"><i class="ion-checkmark-round"></i></span>
+                    </div>
+                    <div class="counter-num">
+                        <p class="counter">12</p>
+                        <span class="counter-text">WORKS COMPLETED</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-lg-3">
+                <div class="counter-box pt-4 pt-md-0">
+                    <div class="counter-ico">
+                        <span class="ico-circle"><i class="ion-ios-calendar-outline"></i></span>
+                    </div>
+                    <div class="counter-num">
+                        <p class="counter">2</p>
+                        <span class="counter-text">YEARS OF EXPERIENCE</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-lg-3">
+                <div class="counter-box pt-4 pt-md-0">
+                    <div class="counter-ico">
+                        <span class="ico-circle"><i class="ion-ios-people"></i></span>
+                    </div>
+                    <div class="counter-num">
+                        <p class="counter">30</p>
+                        <span class="counter-text">TOTAL CLIENTS</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-lg-3">
+                <div class="counter-box pt-4 pt-md-0">
+                    <div class="counter-ico">
+                        <span class="ico-circle"><i class="ion-ribbon-a"></i></span>
+                    </div>
+                    <div class="counter-num">
+                        <p class="counter">1</p>
+                        <span class="counter-text">AWARD WON</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
